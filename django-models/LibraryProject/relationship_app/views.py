86b786eed26e382f74427ffed57a60741603dfa2
GORDONSAMWEL <gordonsamwel@gmail.com>
2025-08-04 16:17:42 +0300

    new commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,5 @@ from .models import Book
 
 # Function-based view to list all books
 def list_books(request):
-    books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
-
+    books = Book.objects.all()  # required: use this exact method
+    return render(request, "relationship_app/list_books.html", {'books': books})
