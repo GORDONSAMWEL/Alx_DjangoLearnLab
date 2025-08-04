9f6a1ef2a103218def2aa19364db8fd24d6f6aed
GORDONSAMWEL <gordonsamwel@gmail.com>
2025-08-04 16:21:18 +0300

    new commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,3 +5,10 @@ from .models import Book
 def list_books(request):
     books = Book.objects.all()  # required: use this exact method
     return render(request, "relationship_app/list_books.html", {'books': books})
+
+
+# Class-based view for library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"  # required
+    context_object_name = "library"  # required
