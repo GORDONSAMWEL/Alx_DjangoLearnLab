09a9ae258ade9d4629985a2a688a6bdaf80fa46f
GORDONSAMWEL <gordonsamwel@gmail.com>
2025-08-05 17:46:09 +0300

    new commit

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/update', views.update_book, name='update_book'),
+    path('books/delete', views.delete_book, name='delete_book'),
 
     path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
     path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
