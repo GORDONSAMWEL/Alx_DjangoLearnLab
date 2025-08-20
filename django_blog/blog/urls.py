ec27a351f09e448214e43a1fc21ee7ca2a37a3f9
GORDONSAMWEL <gordonsamwel@gmail.com>
2025-08-20 18:23:30 +0300

    new commit

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
     # Comment URLs
-    path('posts/<int:pk>/comments/new/', add_comment, name='comment-add'),
+    path('post/<int:pk>/comments/new/', add_comment, name='comment-add'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
