c222599d0a94c07ce50289d5e562065ad8be2cd2
GORDONSAMWEL <gordonsamwel@gmail.com>
2025-08-20 18:07:06 +0300

    new commit

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('profile/', profile, name='profile_short'),
 
     path('posts/', PostListView.as_view(), name='post-list'),              # List all posts
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),      # Create a new post
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'), # View single post
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),  # Edit post
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'), # Delete post
+    path('post/new/', PostCreateView.as_view(), name='post-create'),      # Create a new post
+    path('post/<int:pk>/update/', PostDetailView.as_view(), name='post-detail'), # View single post
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),  # Edit post
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'), # Delete post
 ]
