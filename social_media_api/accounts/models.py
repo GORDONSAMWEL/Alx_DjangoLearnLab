29cd60363ecafb1939467f72f0f1cc11d91418f2
GORDONSAMWEL <gordonsamwel@gmail.com>
2025-08-23 17:27:00 +0300

    new commit

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,16 +1,25 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    profile_picture = models.ImageField(upload_to="profiles/", blank=True, null=True)
+    
     followers = models.ManyToManyField(
-        "self",
+        'self',
         symmetrical=False,
         related_name="following",
         blank=True
     )
 
-    def __str__(self):
-        return self.username
+    def follow(self, user):
+        """Follow another user"""
+        if user != self:
+            self.following.add(user)
+
+    def unfollow(self, user):
+        """Unfollow another user"""
+        self.following.remove(user)
+
+    def is_following(self, user):
+        return self.following.filter(id=user.id).exists()
